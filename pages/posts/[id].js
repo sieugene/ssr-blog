e730b8cc60eaf7b5ed472a97c5631a521310f422
sieugene <evgenii.sidelnikov@unitbean.io>
2020-07-28 12:03:32 +0300

    init

---
+++ pages/posts/[id].js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from './../../components/date';
+export default function Posts({postData}) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+        {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      <Date dateString={postData.date} />
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}

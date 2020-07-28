6acdf88f4c80a8c92920af4cae9cfabe67e645b0
sieugene <evgenii.sidelnikov@unitbean.io>
2020-07-28 16:00:50 +0300

    .

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import Head from "next/head";
-import Link from "next/link";
-import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "./../lib/posts";
-import Date from './../components/date';
-import Layout from "../components/layout";
-import { siteTitle } from './../components/Layout';
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
   return (
@@ -15,36 +14,35 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <section className={utilStyles.headingMd}>…</section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-             <li key={id}>
-             <Link href="/posts/[id]" as={`/posts/${id}`}>
-               <a>{title}</a>
-             </Link>
-             <br />
-             <small>
-               <Date dateString={date} />
-             </small>
-           </li>
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
     </Layout>
-  );
+  )
 }
 
 export async function getStaticProps() {
-  const allPostsData = await getSortedPostsData();
+  const allPostsData = getSortedPostsData()
   return {
     props: {
-      allPostsData,
-    },
-  };
+      allPostsData
+    }
+  }
 }
